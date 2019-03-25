88dca2aec6349b3acd6cd6cf9d4cb907835b8aa6
brentru <robots199@me.com>
2019-03-25 17:17:47 -0400

    update example

--- examples/dymoscale_simpletest.py
+++ examples/dymoscale_simpletest.py
@@ -1,16 +1,15 @@
 import board
 from adafruit_dymoscale import Scale
 
-# initialize the scale's USB and g/oz button
+# initialize the dymo scale
 dymo = Scale(board.D3, board.D4)
 
 while True:
-    try:
-        dymo.get_scale_data()
-        if dymo.units == 'oz':
-            text = "%0.1f oz" % dymo.weight
-        if dymo.units == 'g':
-            text = "%0.1f g" % dymo.weight
-        print(text)
-    except RuntimeError:
-        print("Failed to read data, is scale on?")
+    dymo.get_scale_data()
+    if dymo.units == 'oz':
+        text = "%0.1f oz" % dymo.weight
+    if dymo.units == 'g':
+        text = "%0.1f g" % dymo.weight
+    print(text)
+    # to avoid sleep mode, we'll toggle the units pin
+    dymo.toggle_unit_pin()
